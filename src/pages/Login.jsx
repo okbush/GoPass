9ec3601a92cbd2d3034afde7deb8a7c5cbacb40c
Daph <143109325+okbush@users.noreply.github.com>
2024-12-14 08:55:04 +0800

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,33 @@
+import React from 'react';
 import '../styles/Login.css';
 
-function Login() {
-    return (
-        <div>
-            <h2>Login</h2>
-            <form>
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="Password" />
-                <button type="submit">Login</button>
-            </form>
-        </div>
-    );
-}
+const Login = () => {
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h1>Welcome to <span className="brand-name">GoPass</span> 👋</h1>
+        <p>Please login here</p>
+        <form className="login-form">
+          <div className="form-group">
+            <label>Email Address</label>
+            <input type="email" placeholder="example@email.com" />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input type="password" placeholder="**********" />
+          </div>
+          <div className="form-options">
+            <div>
+              <input type="checkbox" id="rememberMe" />
+              <label htmlFor="rememberMe">Remember Me</label>
+            </div>
+            <a href="/forgot-password">Forgot Password?</a>
+          </div>
+          <button type="submit" className="login-button">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
