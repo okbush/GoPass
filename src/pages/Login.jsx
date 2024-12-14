302a0a05a3da7676c82bfa7dcfa052ba6fa88915
Daph <143109325+okbush@users.noreply.github.com>
2024-12-14 09:06:13 +0800

    Linked with SignUp.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,30 +1,38 @@
+// Login.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Login.css';
 
 const Login = () => {
   return (
     <div className="login-container">
-      <div className="login-box">
-        <h1>Welcome to <span className="brand-name">GoPass</span> 👋</h1>
-        <p>Please login here</p>
-        <form className="login-form">
-          <div className="form-group">
-            <label>Email Address</label>
-            <input type="email" placeholder="example@email.com" />
-          </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input type="password" placeholder="**********" />
-          </div>
-          <div className="form-options">
-            <div>
-              <input type="checkbox" id="rememberMe" />
-              <label htmlFor="rememberMe">Remember Me</label>
+      <div className="login-right">
+        <div className="login-box">
+          <h1>Welcome to GoPass</h1>
+          <p>Please login here</p>
+          <form className="login-form">
+            <div className="form-group">
+              <label>Email Address</label>
+              <input type="email" placeholder="example@email.com" />
             </div>
-            <a href="/forgot-password">Forgot Password?</a>
-          </div>
-          <button type="submit" className="login-button">Login</button>
-        </form>
+            <div className="form-group">
+              <label>Password</label>
+              <input type="password" placeholder="********" />
+            </div>
+            <div className="form-options">
+              <label>
+                <input type="checkbox" /> Remember Me
+              </label>
+              <a href="/forgot-password" className="forgot-password">
+                Forgot Password?
+              </a>
+            </div>
+            <button type="submit" className="login-button">Login</button>
+          </form>
+          <p className="switch-page">
+            Don't have an account? <Link to="/signup">Sign Up</Link>
+          </p>
+        </div>
       </div>
     </div>
   );
